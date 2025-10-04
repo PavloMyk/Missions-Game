aad31104f324a659a30fb0ba0691b04091b28963
PavloMyk <pavlo24022000@gmail.com>
2025-10-04 20:32:45 +0200

    Gun 2

--- Assets/Scriprs/Gun script.cs
+++ Assets/Scriprs/Gun script.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class Gunscript : MonoBehaviour
 {
     public GameObject gun1;
+
+    bool activated;
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            gun1.SetActive(true);
+            gun1.SetActive(activated);
+            activated = !activated; 
         }
     }
 }
